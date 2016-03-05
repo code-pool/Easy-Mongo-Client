b8ad4bf900123c302462a67943efdab4c29c828b
Shamsher <ansarisam47@gmail.com>
2016-03-05 17:53:01 +0530

    Add db create view and controller

--- app/modules/dashboard/controllers/dashboard.controller.js
+++ app/modules/dashboard/controllers/dashboard.controller.js
@@ -2,8 +2,33 @@
 
 angular
  .module('dashboard')
- .controller('DashboardCtrl', ['$scope', '$state', DashboardCtrl]);
+ .controller('DashboardCtrl', ['$scope', '$mdDialog', DashboardCtrl])
+ .controller('SaveDatabaseCtrl', ['$scope', '$mdDialog',SaveDatabaseCtrl]);
 
-function DashboardCtrl($scope, $state) {
+function DashboardCtrl($scope, $mdDialog) {
+
+  $scope.showDailogForDb = function(ev) {
+    
+    $mdDialog.show({
+      controller: SaveDatabaseCtrl,
+      templateUrl: 'app/modules/dashboard/templates/createdb.view.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    });
+
+  }
+}
+
+function SaveDatabaseCtrl($scope, $mdDialog) {
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  $scope.save = function() {
+    console.log($scope.dbname);
+    $mdDialog.hide();
+  }
 
 }
