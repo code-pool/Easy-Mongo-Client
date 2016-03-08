57757a6cfa2422f0128f257a15ae65715b39de13
Pranay Dubey <pranaydubey3009@gmail.com>
2016-03-08 20:51:11 +0530

    Updates on collection list  - Update UI and add pagination

--- app/modules/collection/controllers/collection.controller.js
+++ app/modules/collection/controllers/collection.controller.js
@@ -3,7 +3,7 @@
 angular
  .module('collection')
  .controller('CollectionCtrl', ['$scope', '$mdDialog','collections', 'socket', 'CollectionService', '$stateParams','$state','$rootScope','navigationService', CollectionCtrl])
- .controller('DocumentsCtrl',['$scope','documents','$stateParams','_',DocumentsCtrl]);
+ .controller('DocumentsCtrl',['$mdEditDialog', '$q', '$scope', '$timeout','$stateParams','documents',DocumentsCtrl]);
 
 function CollectionCtrl($scope, $mdDialog, collections, socket, CollectionService, $stateParams,$state,$rootScope,navigationService) {
 
@@ -64,7 +64,7 @@ function CollectionCtrl($scope, $mdDialog, collections, socket, CollectionServic
 
       navigationService.remove('home.collection.documents',{'database':$stateParams.database,'collection' : colName});
       $rootScope.$broadcast('notification',{'msg' : msg,'key' : key,'complete': false,'finished' : finished});
-      
+
       CollectionService.delete($stateParams.database, colName);
 
     }, function() {
@@ -90,8 +90,8 @@ function CollectionCtrl($scope, $mdDialog, collections, socket, CollectionServic
   };
 }
 
-function DocumentsCtrl($scope,documents,$stateParams,_){
-  
+function DocumentsCtrl($mdEditDialog, $q, $scope, $timeout,$stateParams,documents){
+
   $scope.collection_name = $stateParams.collection;
   var keys = Object.keys(documents[0]);
   keys = _.reject(keys,function(keyVal){
@@ -99,6 +99,85 @@ function DocumentsCtrl($scope,documents,$stateParams,_){
   });
   $scope.keys = keys;
   $scope.rows = documents;
+
+$scope.selected = [];
+  
+  $scope.options = {
+    autoSelect: true,
+    boundaryLinks: true,
+    largeEditDialog: true,
+    pageSelector: true,
+    rowSelection: true
+  };
+  
+  $scope.query = {
+    order: keys[0],
+    limit: 5,
+    page: 1
+  };
+  
+  
+  $scope.editComment = function (event, dessert) {
+    event.stopPropagation(); // in case autoselect is enabled
+    
+    var editDialog = {
+      modelValue: dessert.comment,
+      placeholder: 'Add a comment',
+      save: function (input) {
+        if(input.$modelValue === 'Donald Trump') {
+          return $q.reject();
+        }
+        if(input.$modelValue === 'Bernie Sanders') {
+          return dessert.comment = 'FEEL THE BERN!'
+        }
+        dessert.comment = input.$modelValue;
+      },
+      targetEvent: event,
+      title: 'Add a comment',
+      validators: {
+        'md-maxlength': 30
+      }
+    };
+    
+    var promise;
+    
+    if($scope.options.largeEditDialog) {
+      promise = $mdEditDialog.large(editDialog);
+    } else {
+      promise = $mdEditDialog.small(editDialog);
+    }
+    
+    promise.then(function (ctrl) {
+      var input = ctrl.getInput();
+      
+      input.$viewChangeListeners.push(function () {
+        input.$setValidity('test', input.$modelValue !== 'test');
+      });
+    });
+  };
+  
+  $scope.getTypes = function () {
+    return ['Candy', 'Ice cream', 'Other', 'Pastry'];
+  };
+  
+  $scope.loadStuff = function () {
+    $scope.promise = $timeout(function () {
+      // loading
+    }, 2000);
+  }
+  
+  $scope.logItem = function (item) {
+    console.log(item.name, 'was selected');
+  };
+  
+  $scope.logOrder = function (order) {
+    console.log('order: ', order);
+  };
+  
+  $scope.logPagination = function (page, limit) {
+    console.log('page: ', page);
+    console.log('limit: ', limit);
+  }
 }
 
 function CreateCollectionCtrl($scope,$mdDialog,collections,_,CollectionService,$stateParams,$rootScope){
